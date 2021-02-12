947f0109e41e8f67e90bf2044de9c8ca3618b1da
wangfuzheng <1677980708@qq.com>
2021-02-12 20:03:19 +0800

    update WindowTest3_EventTimeWindow.java

--- src/main/java/com/erxi/apitest/window/WindowTest3_EventTimeWindow.java
+++ src/main/java/com/erxi/apitest/window/WindowTest3_EventTimeWindow.java
@@ -1,6 +1,7 @@
 package com.erxi.apitest.window;
 
 import com.erxi.apitest.beans.SensorReading;
+import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -16,11 +17,13 @@ import org.apache.flink.util.OutputTag;
 public class WindowTest3_EventTimeWindow {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);// 设置流式时间语义
+        env.getConfig().setAutoWatermarkInterval(100);
 
         // socket文本流
         DataStream<String> inputStream = env.socketTextStream("localhost", 7777);
 
-        // 转换成 SensorReading 类型
+        // 转换成 SensorReading 类型，设置时间戳和 watermark
         DataStream<SensorReading> dataStream = inputStream.map(line -> {
             String[] fields = line.split(",");
             return new SensorReading(fields[0], new Long(fields[1]), new Double(fields[2]));
