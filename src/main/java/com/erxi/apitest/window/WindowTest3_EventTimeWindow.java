eecc6fe21d08294ba745ee6b5d9f9bc656d93e9c
wangfuzheng <1677980708@qq.com>
2021-02-12 15:52:24 +0800

    create WindowTest3_EventTimeWindow.java

---
+++ src/main/java/com/erxi/apitest/window/WindowTest3_EventTimeWindow.java
@@ -0,0 +1,56 @@
+package com.erxi.apitest.window;
+
+import com.erxi.apitest.beans.SensorReading;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.timestamps.AscendingTimestampExtractor;
+import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.OutputTag;
+
+/**
+ * @author AKA二夕
+ * @create 2021-02-12 15:39
+ */
+public class WindowTest3_EventTimeWindow {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        // socket文本流
+        DataStream<String> inputStream = env.socketTextStream("localhost", 7777);
+
+        // 转换成 SensorReading 类型
+        DataStream<SensorReading> dataStream = inputStream.map(line -> {
+            String[] fields = line.split(",");
+            return new SensorReading(fields[0], new Long(fields[1]), new Double(fields[2]));
+        })
+                // 升序数据设置时间和 watermark
+                .assignTimestampsAndWatermarks(new AscendingTimestampExtractor<SensorReading>() {
+                    @Override
+                    public long extractAscendingTimestamp(SensorReading element) {
+                        return element.getTimestamp() * 1000L;
+                    }
+                })
+                // 乱序数据设置时间戳和 watermark
+                .assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor<SensorReading>(Time.seconds(2)) {
+                    @Override
+                    public long extractTimestamp(SensorReading element) {
+                        return element.getTimestamp() * 1000L;
+                    }
+                });
+
+        OutputTag<SensorReading> outputTag = new OutputTag<SensorReading>("late") {
+        };
+
+        // 基于事件时间的开窗聚合，统计 15 秒内温度的最小值
+        SingleOutputStreamOperator<SensorReading> minTempStream = dataStream.keyBy("id")
+                .timeWindow(Time.seconds(15))
+                .allowedLateness(Time.minutes(1))
+                .sideOutputLateData(outputTag)
+                .minBy("temperament");
+
+        minTempStream.print("minTemp");
+        minTempStream.getSideOutput(outputTag).print("late");
+    }
+}
