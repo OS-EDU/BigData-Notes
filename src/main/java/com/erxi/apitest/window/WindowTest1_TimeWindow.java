206ccbd4aab1fed4e831c1f9255c57a4ac84a2c3
wangfuzheng <1677980708@qq.com>
2021-03-11 23:17:35 +0800

    update WindowTest1_TimeWindow.java

--- src/main/java/com/erxi/apitest/window/WindowTest1_TimeWindow.java
+++ src/main/java/com/erxi/apitest/window/WindowTest1_TimeWindow.java
@@ -20,6 +20,7 @@ import org.apache.flink.util.OutputTag;
  */
 public class WindowTest1_TimeWindow {
     public static void main(String[] args) throws Exception {
+        // 创建执行环境，并设置并行度为 1
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
 
@@ -33,20 +34,31 @@ public class WindowTest1_TimeWindow {
         });
 
         // 开窗测试
-        // 1. 增量聚合函数
+        // 1. 增量聚合函数，用于统计某固定时间段内输入数据的个数，每条数据到来就进行计算，保持一个简单的状态。
         DataStream<Integer> resultStream1 = dataStream.keyBy("id")
-                .timeWindow(Time.seconds(15))
+                .timeWindow(Time.seconds(15)) // 开启一个 15s 的窗口
                 .aggregate(new AggregateFunction<SensorReading, Integer, Integer>() {
                     @Override
                     public Integer createAccumulator() {
                         return 0;
                     }
 
+                    /** 将给定的值输入到累加器中，并返回经过计算之后得出的结果
+                     *
+                     * @param value 需要增加的值
+                     * @param accumulator 用于增加的值
+                     * @return 每输入一个 value ，则 accumulator + 1，并返回结果
+                     */
                     @Override
                     public Integer add(SensorReading value, Integer accumulator) {
                         return accumulator + 1;
                     }
 
+                    /**
+                     * 获取经过累加器增量聚合之后的结果
+                     * @param accumulator 聚合的累加器
+                     * @return 最终聚合的结果
+                     */
                     @Override
                     public Integer getResult(Integer accumulator) {
                         return accumulator;
@@ -58,15 +70,23 @@ public class WindowTest1_TimeWindow {
                     }
                 });
 
-        // 2、全窗口函数
-        SingleOutputStreamOperator<Tuple3<String, Long, Integer>> resultStream2 = dataStream.keyBy("id")
+        // 2、全窗口函数：先把窗口所有数据收集起来，等到计算的时候会遍历所有数据。
+        DataStream<Tuple3<String, Long, Integer>> resultStream2 = dataStream.keyBy("id")
                 .timeWindow(Time.seconds(15))
                 .apply(new WindowFunction<SensorReading, Tuple3<String, Long, Integer>, Tuple, TimeWindow>() {
+                    /**
+                     *
+                     * @param tuple 当前的 key
+                     * @param window 当前的窗口类型
+                     * @param input 输入
+                     * @param out 输出
+                     * @throws Exception 抛出异常
+                     */
                     @Override
                     public void apply(Tuple tuple, TimeWindow window, Iterable<SensorReading> input, Collector<Tuple3<String, Long, Integer>> out) throws Exception {
                         String id = tuple.getField(0);
-                        Long windowEnd = window.getEnd();
-                        Integer count = IteratorUtils.toList(input.iterator()).size();
+                        Long windowEnd = window.getEnd(); // 此窗口的独占结束时间戳
+                        Integer count = IteratorUtils.toList(input.iterator()).size(); // 获取当前 sensor 的个数
                         out.collect(new Tuple3<>(id, windowEnd, count));
                     }
                 });
@@ -83,7 +103,7 @@ public class WindowTest1_TimeWindow {
 
         sumStream.getSideOutput(outputTag).print("late");
 
-        resultStream2.print();
+        resultStream2.print("window");
 
         env.execute();
     }
