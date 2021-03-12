23e17f71ca4c69041bb2efe5dc9f2cce76e57f7d
wangfuzheng <1677980708@qq.com>
2021-03-12 15:12:57 +0800

    update WindowTest2_CountWindow.java : add comment

--- src/main/java/com/erxi/apitest/window/WindowTest2_CountWindow.java
+++ src/main/java/com/erxi/apitest/window/WindowTest2_CountWindow.java
@@ -1,10 +1,17 @@
 package com.erxi.apitest.window;
 
 import com.erxi.apitest.beans.SensorReading;
+import org.apache.flink.api.common.functions.AggregateFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * @class WindowTest2_CountWindow 测试计数窗口
+ * eg：窗口大小 10，步长 2，那么前5次输出时，窗口内的元素个数分别是（2，4，6，8，10），再往后就是10个为一个窗口了。
+ * 这里测试获取每个窗口的平均温度值
+ */
 
 /**
  * @author AKA二夕
@@ -26,7 +33,7 @@ public class WindowTest2_CountWindow {
 
         // 开计数窗口测试
         SingleOutputStreamOperator<Double> avgTempResultStream = dataStream.keyBy("id")
-                .countWindow(10, 2)
+                .countWindow(10, 2) // 创建滑动计数窗口，每 2 个元素统计过去 2 个元素
                 .aggregate(new MyAvgTemp());
 
         avgTempResultStream.print();
@@ -34,8 +41,19 @@ public class WindowTest2_CountWindow {
         env.execute();
     }
 
-    // 自定义 MyAvgTemp
-    public static class MyAvgTemp implements org.apache.flink.api.common.functions.AggregateFunction<SensorReading, Tuple2<Double, Integer>, Double> {
+    /**
+     * 自定义 MyAvgTemp
+     *
+     * @SensorReading 传入所需聚合元素
+     * @Tuple2<Double,Integter> 聚合过程中的状态
+     * @Double 最后聚合之后的结果
+     */
+    public static class MyAvgTemp implements AggregateFunction<SensorReading, Tuple2<Double, Integer>, Double> {
+        /**
+         * 创建聚合状态
+         *
+         * @return 返回一个二元组
+         */
         @Override
         public Tuple2<Double, Integer> createAccumulator() {
             return new Tuple2<>(0.0, 0);
@@ -43,6 +61,7 @@ public class WindowTest2_CountWindow {
 
         @Override
         public Tuple2<Double, Integer> add(SensorReading value, Tuple2<Double, Integer> accumulator) {
+            // 温度累加求和，当前统计的温度个数 +1
             return new Tuple2<>(accumulator.f0 + value.getTemperature(), accumulator.f1 + 1);
         }
 
